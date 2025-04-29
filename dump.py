b4b34a245c54349215be35122f06a48013ac0a00
Rahul <rpandey5000@gmail.com>
2025-04-30 01:35:00 +0530

    fix for spawning new process with error, using PID instead
    iOS was unable to attach to app because it was spawning it instead. Similar issue is also seen with frida android (in my case), hence py was modified to accommodate for PID

--- dump.py
+++ dump.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Author : AloneMonkey
+# Author : AloneMonkey (modified to support PID attach fallback)
 # blog: www.alonemonkey.com
 
-from __future__ import print_function
-from __future__ import unicode_literals
+from __future__ import print_function, unicode_literals
 import sys
 import codecs
 import frida
@@ -68,23 +67,19 @@ def get_usb_iphone():
             device = devices[0]
 
     device_manager.off('changed', on_changed)
-
     return device
 
 
 def generate_ipa(path, display_name):
     ipa_filename = display_name + '.ipa'
-
     print('Generating "{}"'.format(ipa_filename))
     try:
         app_name = file_dict['app']
-
         for key, value in file_dict.items():
             from_dir = os.path.join(path, key)
             to_dir = os.path.join(path, app_name, value)
             if key != 'app':
                 shutil.move(from_dir, to_dir)
-
         target_dir = './' + PAYLOAD_DIR
         zip_args = ('zip', '-qr', os.path.join(os.getcwd(), ipa_filename), target_dir)
         subprocess.check_call(zip_args, cwd=TEMP_DIR)
@@ -93,14 +88,15 @@ def generate_ipa(path, display_name):
         print(e)
         finished.set()
 
+
 def on_message(message, data):
-    t = tqdm(unit='B',unit_scale=True,unit_divisor=1024,miniters=1)
+    t = tqdm(unit='B', unit_scale=True, unit_divisor=1024, miniters=1)
     last_sent = [0]
 
     def progress(filename, size, sent):
         baseName = os.path.basename(filename)
         if IS_PY2 or isinstance(baseName, bytes):
-            t.desc = baseName.decode("utf-8")
+            t.desc = baseName.decode('utf-8')
         else:
             t.desc = baseName
         t.total = size
@@ -112,251 +108,189 @@ def on_message(message, data):
         if 'dump' in payload:
             origin_path = payload['path']
             dump_path = payload['dump']
-
             scp_from = dump_path
             scp_to = PAYLOAD_PATH + '/'
-
-            with SCPClient(ssh.get_transport(), progress = progress, socket_timeout = 60) as scp:
+            with SCPClient(ssh.get_transport(), progress=progress, socket_timeout=60) as scp:
                 scp.get(scp_from, scp_to)
-
             chmod_dir = os.path.join(PAYLOAD_PATH, os.path.basename(dump_path))
-            chmod_args = ('chmod', '655', chmod_dir)
-            try:
-                subprocess.check_call(chmod_args)
-            except subprocess.CalledProcessError as err:
-                print(err)
-
+            subprocess.call(('chmod', '655', chmod_dir))
             index = origin_path.find('.app/')
             file_dict[os.path.basename(dump_path)] = origin_path[index + 5:]
-
         if 'app' in payload:
             app_path = payload['app']
-
             scp_from = app_path
             scp_to = PAYLOAD_PATH + '/'
-            with SCPClient(ssh.get_transport(), progress = progress, socket_timeout = 60) as scp:
+            with SCPClient(ssh.get_transport(), progress=progress, socket_timeout=60) as scp:
                 scp.get(scp_from, scp_to, recursive=True)
-
             chmod_dir = os.path.join(PAYLOAD_PATH, os.path.basename(app_path))
-            chmod_args = ('chmod', '755', chmod_dir)
-            try:
-                subprocess.check_call(chmod_args)
-            except subprocess.CalledProcessError as err:
-                print(err)
-
+            subprocess.call(('chmod', '755', chmod_dir))
             file_dict['app'] = os.path.basename(app_path)
-
         if 'done' in payload:
             finished.set()
     t.close()
+
 
 def compare_applications(a, b):
     a_is_running = a.pid != 0
     b_is_running = b.pid != 0
     if a_is_running == b_is_running:
-        if a.name > b.name:
-            return 1
-        elif a.name < b.name:
-            return -1
-        else:
-            return 0
-    elif a_is_running:
-        return -1
-    else:
-        return 1
+        return (a.name > b.name) - (a.name < b.name)
+    return -1 if a_is_running else 1
 
 
 def cmp_to_key(mycmp):
-    """Convert a cmp= function into a key= function"""
-
     class K:
-        def __init__(self, obj):
-            self.obj = obj
-
-        def __lt__(self, other):
-            return mycmp(self.obj, other.obj) < 0
-
-        def __gt__(self, other):
-            return mycmp(self.obj, other.obj) > 0
-
-        def __eq__(self, other):
-            return mycmp(self.obj, other.obj) == 0
-
-        def __le__(self, other):
-            return mycmp(self.obj, other.obj) <= 0
-
-        def __ge__(self, other):
-            return mycmp(self.obj, other.obj) >= 0
-
-        def __ne__(self, other):
-            return mycmp(self.obj, other.obj) != 0
-
+        def __init__(self, obj): self.obj = obj
+        def __lt__(self, other): return mycmp(self.obj, other.obj) < 0
+        def __gt__(self, other): return mycmp(self.obj, other.obj) > 0
+        def __eq__(self, other): return mycmp(self.obj, other.obj) == 0
+        def __le__(self, other): return mycmp(self.obj, other.obj) <= 0
+        def __ge__(self, other): return mycmp(self.obj, other.obj) >= 0
+        def __ne__(self, other): return mycmp(self.obj, other.obj) != 0
     return K
 
 
 def get_applications(device):
     try:
-        applications = device.enumerate_applications()
+        return device.enumerate_applications()
     except Exception as e:
-        sys.exit('Failed to enumerate applications: %s' % e)
-
-    return applications
+        sys.exit(f'Failed to enumerate applications: {e}')
 
 
 def list_applications(device):
-    applications = get_applications(device)
-
-    if len(applications) > 0:
-        pid_column_width = max(map(lambda app: len('{}'.format(app.pid)), applications))
-        name_column_width = max(map(lambda app: len(app.name), applications))
-        identifier_column_width = max(map(lambda app: len(app.identifier), applications))
-    else:
-        pid_column_width = 0
-        name_column_width = 0
-        identifier_column_width = 0
-
-    header_format = '%' + str(pid_column_width) + 's  ' + '%-' + str(name_column_width) + 's  ' + '%-' + str(
-        identifier_column_width) + 's'
-    print(header_format % ('PID', 'Name', 'Identifier'))
-    print('%s  %s  %s' % (pid_column_width * '-', name_column_width * '-', identifier_column_width * '-'))
-    line_format = '%' + str(pid_column_width) + 's  ' + '%-' + str(name_column_width) + 's  ' + '%-' + str(
-        identifier_column_width) + 's'
-    for application in sorted(applications, key=cmp_to_key(compare_applications)):
-        if application.pid == 0:
-            print(line_format % ('-', application.name, application.identifier))
-        else:
-            print(line_format % (application.pid, application.name, application.identifier))
+    apps = get_applications(device)
+    pid_w = max((len(str(a.pid)) for a in apps), default=0)
+    name_w = max((len(a.name) for a in apps), default=0)
+    id_w = max((len(a.identifier) for a in apps), default=0)
+    print(f"{'PID':>{pid_w}}  {'Name':<{name_w}}  {'Identifier':<{id_w}}")
+    print(f"{'-'*pid_w}  {'-'*name_w}  {'-'*id_w}")
+    for app in sorted(apps, key=cmp_to_key(compare_applications)):
+        pid_s = '-' if app.pid == 0 else str(app.pid)
+        print(f"{pid_s:>{pid_w}}  {app.name:<{name_w}}  {app.identifier:<{id_w}}")
 
 
 def load_js_file(session, filename):
-    source = ''
     with codecs.open(filename, 'r', 'utf-8') as f:
-        source = source + f.read()
+        source = f.read()
     script = session.create_script(source)
     script.on('message', on_message)
     script.load()
-
     return script
 
 
 def create_dir(path):
-    path = path.strip()
-    path = path.rstrip('\\')
-    if os.path.exists(path):
-        shutil.rmtree(path)
-    try:
-        os.makedirs(path)
-    except os.error as err:
-        print(err)
+    if os.path.exists(path): shutil.rmtree(path)
+    os.makedirs(path, exist_ok=True)
 
 
 def open_target_app(device, name_or_bundleid):
-    print('Start the target app {}'.format(name_or_bundleid))
-
-    pid = ''
+    print(f"Start or attach to target '{name_or_bundleid}'")
     session = None
+    pid = None
     display_name = ''
     bundle_identifier = ''
-    for application in get_applications(device):
-        if name_or_bundleid == application.identifier or name_or_bundleid == application.name:
-            pid = application.pid
-            display_name = application.name
-            bundle_identifier = application.identifier
 
-    try:
-        if not pid:
+    # PID fallback
+    if name_or_bundleid.isdigit():
+        pid = int(name_or_bundleid)
+        try:
+            session = device.attach(pid)
+            print(f"  attached by PID = {pid}")
+            return session, str(pid), None
+        except Exception as e:
+            print(f"  ❌ failed to attach to PID {pid}: {e}")
+            sys.exit(1)
+
+    # find running app
+    for app in get_applications(device):
+        if name_or_bundleid in (app.identifier, app.name):
+            pid = app.pid
+            display_name = app.name
+            bundle_identifier = app.identifier
+            break
+
+    # spawn if not running
+    if not pid and bundle_identifier:
+        try:
             pid = device.spawn([bundle_identifier])
+            print(f"  spawned, pid = {pid}")
             session = device.attach(pid)
             device.resume(pid)
-        else:
+        except Exception as e:
+            print(f"  spawn failed: {e}")
+
+    # attach if needed
+    if not session and pid:
+        try:
+            print("  attaching to existing process…")
             session = device.attach(pid)
-    except Exception as e:
-        print(e) 
+            print(f"  attached, pid = {session.pid}")
+        except Exception as e:
+            print(f"  attach failed: {e}")
 
     return session, display_name, bundle_identifier
 
 
 def start_dump(session, ipa_name):
-    print('Dumping {} to {}'.format(display_name, TEMP_DIR))
-
+    print(f"Dumping {ipa_name} to {TEMP_DIR}")
     script = load_js_file(session, DUMP_JS)
     script.post('dump')
     finished.wait()
-
     generate_ipa(PAYLOAD_PATH, ipa_name)
-
     if session:
         session.detach()
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='frida-ios-dump (by AloneMonkey v2.0)')
-    parser.add_argument('-l', '--list', dest='list_applications', action='store_true', help='List the installed apps')
-    parser.add_argument('-o', '--output', dest='output_ipa', help='Specify name of the decrypted IPA')
-    parser.add_argument('-H', '--host', dest='ssh_host', help='Specify SSH hostname')
-    parser.add_argument('-p', '--port', dest='ssh_port', help='Specify SSH port')
-    parser.add_argument('-u', '--user', dest='ssh_user', help='Specify SSH username')
-    parser.add_argument('-P', '--password', dest='ssh_password', help='Specify SSH password')
-    parser.add_argument('-K', '--key_filename', dest='ssh_key_filename', help='Specify SSH private key file path')
-    parser.add_argument('target', nargs='?', help='Bundle identifier or display name of the target app')
+    parser = argparse.ArgumentParser(description='frida-ios-dump (modified with PID attach)')
+    parser.add_argument('-l', '--list', action='store_true', dest='list_applications', help='List installed apps')
+    parser.add_argument('-o', '--output', dest='output_ipa', help='Name of the decrypted IPA')
+    parser.add_argument('-H', '--host', dest='ssh_host', help='SSH hostname')
+    parser.add_argument('-p', '--port', dest='ssh_port', help='SSH port')
+    parser.add_argument('-u', '--user', dest='ssh_user', help='SSH username')
+    parser.add_argument('-P', '--password', dest='ssh_password', help='SSH password')
+    parser.add_argument('-K', '--key_filename', dest='ssh_key_filename', help='SSH key file path')
+    parser.add_argument('target', nargs='?', help='Bundle ID, app name, or PID of target')
 
     args = parser.parse_args()
-
-    exit_code = 0
-    ssh = None
-
-    if not len(sys.argv[1:]):
+    if not args.target and not args.list_applications:
         parser.print_help()
-        sys.exit(exit_code)
+        sys.exit(0)
+
+    # override SSH settings
+    if args.ssh_host: Host = args.ssh_host
+    if args.ssh_port: Port = int(args.ssh_port)
+    if args.ssh_user: User = args.ssh_user
+    if args.ssh_password: Password = args.ssh_password
+    if args.ssh_key_filename: KeyFileName = args.ssh_key_filename
 
     device = get_usb_iphone()
-
-    if args.list_applications:
-        list_applications(device)
-    else:
-        name_or_bundleid = args.target
-        output_ipa = args.output_ipa
-        # update ssh args
-        if args.ssh_host:
-            Host = args.ssh_host
-        if args.ssh_port:
-            Port = int(args.ssh_port)
-        if args.ssh_user:
-            User = args.ssh_user
-        if args.ssh_password:
-            Password = args.ssh_password
-        if args.ssh_key_filename:
-            KeyFileName = args.ssh_key_filename
-
-        try:
-            ssh = paramiko.SSHClient()
+    ssh = None
+    exit_code = 0
+    try:
+        if args.list_applications:
+            list_applications(device)
+        else:
+            ssh = SSHClient()
             ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
             ssh.connect(Host, port=Port, username=User, password=Password, key_filename=KeyFileName)
-
             create_dir(PAYLOAD_PATH)
-            (session, display_name, bundle_identifier) = open_target_app(device, name_or_bundleid)
-            if output_ipa is None:
-                output_ipa = display_name
-            output_ipa = re.sub('\.ipa$', '', output_ipa)
-            if session:
-                start_dump(session, output_ipa)
-        except paramiko.ssh_exception.NoValidConnectionsError as e:
-            print(e)
-            print('Try specifying -H/--hostname and/or -p/--port')
-            exit_code = 1
-        except paramiko.AuthenticationException as e:
-            print(e)
-            print('Try specifying -u/--username and/or -P/--password')
-            exit_code = 1
-        except Exception as e:
-            print('*** Caught exception: %s: %s' % (e.__class__, e))
-            traceback.print_exc()
-            exit_code = 1
-
-    if ssh:
-        ssh.close()
-
-    if os.path.exists(PAYLOAD_PATH):
-        shutil.rmtree(PAYLOAD_PATH)
-
-    sys.exit(exit_code)
+            session, display_name, bundle_identifier = open_target_app(device, args.target)
+            ipa_name = args.output_ipa or display_name or args.target
+            start_dump(session, re.sub(r'\.ipa$', '', ipa_name))
+    except paramiko.ssh_exception.NoValidConnectionsError as e:
+        print(e)
+        print('Try specifying -H/--host and/or -p/--port')
+        exit_code = 1
+    except paramiko.AuthenticationException as e:
+        print(e)
+        print('Try specifying -u/--user and/or -P/--password')
+        exit_code = 1
+    except Exception as e:
+        print(f'*** Caught exception: {e.__class__.__name__}: {e}')
+        traceback.print_exc()
+        exit_code = 1
+    finally:
+        if ssh: ssh.close()
+        if os.path.exists(PAYLOAD_PATH): shutil.rmtree(PAYLOAD_PATH)
+        sys.exit(exit_code)
